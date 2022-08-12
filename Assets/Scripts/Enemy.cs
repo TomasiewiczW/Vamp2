d3efb6f0109bd3a9c1b09fdac315fae0e0cf79c7
TomasiewiczW <wojtarek@vp.pl>
2022-08-12 22:44:06 +0200

    Bat enemy added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform targetDestination;
+    [SerializeField] float speed;
+
+    Rigidbody2D rgdbd2d;
+
+    private void Awake()
+    {
+        rgdbd2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = (targetDestination.position - transform.position).normalized;
+        rgdbd2d.velocity = direction * speed;
+
+    }
+}
